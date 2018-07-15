39d0aef17da5fd800e45f6ef149b7eda7efd1aed
Daniel Shapiro <dan@Daniels-MacBook-Pro.local>
2018-07-15 16:09:35 -0400

    store a mapping of wagers

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,30 +2,42 @@ pragma solidity ^0.4.24;
 
 contract Bank {
 
-  enum Goals { LoseWeight }
+  enum Goals { ChangeWeight }
 
   struct Wager {
     uint expiration; //timestamp after which the wager needs to be evaluated
     uint target; //target of person attempting to meet goal
     Goals goal; //fitness goal of person
     uint wagerAmount; //amount this person wagered
-    address who; //address of person attempting to meet goal
   }
 
-  Wager[] public wagers;
-  
-  event WagerCreated(uint, uint, Goals, uint, address);
+  mapping(address => Wager[]) private wagers;
 
-  function createWager(uint _expiration, uint _target, Goals _goal) public payable {
+  event WagerCreated(uint, uint, Goals, uint);
+
+  function createWager(uint _expirationInDays, uint _target, Goals _goal) public payable {
     // In _days, _who will _goal to _target
     //e.g. in 5 days, Jeff will lose weight to 180 lbs
-    emit WagerCreated(_expiration, _target, _goal, msg.value, msg.sender);
-    wagers.push(Wager(_expiration, _target, _goal, msg.value, msg.sender));
+
+    wagers[msg.sender].push(Wager(_expirationInDays, _target, _goal, msg.value));
+    emit WagerCreated(_expirationInDays, _target, _goal, msg.value);
   }
 
-  function getWagers() returns (uint) {
-    return wagers.length;
+  function getWagers() returns (uint[], uint[], Goals[], uint[]) {
+    uint[] memory expirations = new uint[](wagers[msg.sender].length);
+    uint[] memory targets = new uint[](wagers[msg.sender].length);
+    Goals[] memory goals = new Goals[](wagers[msg.sender].length);
+    uint[] memory values = new uint[](wagers[msg.sender].length);
+
+    for (uint ii = 0; ii < wagers[msg.sender].length; ii++) {
+        Wager memory wager = wagers[msg.sender][ii];
+        expirations[ii] = wager.expiration;
+        targets[ii] = wager.target;
+        goals[ii] = wager.goal;
+        values[ii] = wager.wagerAmount;
+    }
+
+    return (expirations, targets, goals, values);
   }
 
 }
-
