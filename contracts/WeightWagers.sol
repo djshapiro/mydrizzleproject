b215b889abfb9d9cfd623ecc402723e8e6d7642a
Daniel Shapiro <dan@Daniels-MacBook-Pro.local>
2018-07-22 14:30:52 -0400

    save point while on plane

--- contracts/WeightWagers.sol
+++ contracts/WeightWagers.sol
@@ -4,7 +4,9 @@ import "installed_contracts/oraclize-api/contracts/usingOraclize.sol";
 
 contract WeightWagers is usingOraclize{
 
-  enum Goals { ChangeWeight }
+  enum Goals { LoseWeight }
+
+  uint rewardMultiplier;
 
   struct Wager {
     uint expiration; //timestamp after which the wager needs to be evaluated
@@ -16,23 +18,55 @@ contract WeightWagers is usingOraclize{
     uint startValue; //the starting value of the fitness goal of the person
   }
 
-  // The official mapping of all active wagers
+  struct VerifyingWager {
+    address wagerer; //the address of the person making the wager
+    uint wagerIndex; //the index of the wager in the wagers array - we have to
+                     //track this so the __callback function knows which wager
+                     //to delete once the wager has been verified
+  }
+
+  // The official mapping of all activated wagers
   mapping(address => Wager[]) private wagers;
 
   // The mapping used to keep track of wagers as we
-  // wait for the oraclized scale data to return
-  mapping(bytes32 => Wager) private activeCalls;
+  // wait for the oraclized scale data to return so
+  // that we can officially activate the wager
+  mapping(bytes32 => Wager) private wagersBeingActivated;
+
+  // The mapping used to keep track of wagers as we
+  // wait for the oraclized scale data to return so
+  // that we can officially verify the wager 
+  mapping(bytes32 => VerifyingWager) private wagersBeingVerified;
 
   // event for when a user attempts to create a wager
   event WagerCreated(uint expiration, uint targetValue, Goals goal, uint wagerAmount, string smartScaleID);
   // event for when the oraclized smart scale returns
   // data for a wager that a user is trying to create
-  event WagerActivated(bytes32 myid);
+  event WagerActivated(address wagerer, uint wagerAmount);
+  // event for when a user attempts to verify a wager
+  event WagerBeingVerified(address verifierAddress, uint wagerIndex);
+  // event for when a wager is verified
+  event WagerVerified(address wagerer, uint wagerAmount);
+  // event for when a wager is expired - called after a user
+  // attempts to verify a wager and the contract discovers
+  // that the wager has expired
+  event WagerExpired(address wagerer, uint wagerAmount);
+  // event for when the __callback recieves an invalid wager id
+  event InvalidWager(bytes32 myid);
+  // event for when the verify callback recieves a valid
+  // wager but one where the user hasn't actually achieved
+  // their goal yet
+  event WagerUnchanged(bytes32 myid);
+  // event for when the verify callback attempts to verify
+  // a wager whose goal is an integer outside the enum's range
+  event InvalidGoal(bytes32 myid);
 
   function WeightWagers() {
-    OAR = OraclizeAddrResolverI(0x79FBC344D7644a8DebD395d599588e11Bc8b737C);
+    rewardMultiplier = 100;
+    OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
   }
 
+  //The user calls this function when they want to create a wager
   function createWager(uint _expirationInDays, uint _target, Goals _goal, string _smartScaleID) public payable {
     //This payable function should automatically receive the ether
     //which is fine! because now if there are problems we can just revert.
@@ -42,18 +76,57 @@ contract WeightWagers is usingOraclize{
     //e.g. in 5 days, Jeff will lose weight to 180 lbs
 
     bytes32 myID = oraclize_query("URL", "json(https://api.coinbase.com/v2/prices/ETH-USD/spot).data.amount");
-    activeCalls[myID] = Wager(_expirationInDays, _target, _goal, msg.value, _smartScaleID, msg.sender, 0);
+    wagersBeingActivated[myID] = Wager(_expirationInDays, _target, _goal, msg.value, _smartScaleID, msg.sender, 0);
     emit WagerCreated(_expirationInDays, _target, _goal, msg.value, _smartScaleID);
-    //wagers[msg.sender].push(Wager(_expirationInDays, _target, _goal, msg.value, _smartScaleID));
   }
   
   function __callback(bytes32 myid, string result) public {
+
     if (msg.sender != oraclize_cbAddress()) revert();
-    Wager memory newWager = activeCalls[myid];
-    //DJSFIXME Delete wager from activeCalls
+    //DJSFIXME If statement to see where myid is.
+    //DJSFIXME If myid is in wagersBeingActivated
+    Wager memory newWager = wagersBeingActivated[myid];
+    //DJSFIXME Delete wager from wagersBeingActivated
     newWager.startValue = parseInt(result);
     wagers[newWager.wagerer].push(newWager);
-    emit WagerActivated(myid);
+    emit WagerActivated(newWager.wagerer, newWager.wagerAmount);
+    //DJSFIXME Maybe include a modifier to delete the wager from wagersBeingActivated
+    //         on function exit no matter what, so that wagersBeingActivated doesn't
+    //         slowly bloat into tons of data that no one needs
+
+    //DJSFIXME If myid is in wagersBeingVerified
+    //VerifyingWager memory myVerifyingWager = wagersBeingVerified[myid];
+    //Wager memory wagerToVerify = wagers[myVerifyingWager.wagerer][myVerifyingWager.wagerIndex];
+    //If wagerToVerify.goal == Goals.LoseWeight
+    //If parseInt(result) < wagerToVerify.startValue;
+    //DJSFIXME then Send wagerToVerify.value * rewardMultipier / 100 to wagerToVerify.wagerer.
+    //DJSFIXME emit WagerVerified(wagerToVerify.wagerer, wagerToVerify.wagerAmount);
+    //DJSFIXME else (they didn't lose the weight)
+    //DJSFIXME then do nothing (give them a chance to keep losing weight until the wager expires)
+    //DJSFIXME emit WagerUnchanged(myid);
+    //DJSFIXME else (the goal of this wager doesn't match any existing Goals)
+    //DJSFIXME remove the wager from wagers
+    //DJSFIXME emit InvalidGoal(myid);
+    
+    //DJSFIXME Maybe include a modifier to delete the wager from wagersBeingVerified
+    //         on function exit no matter what, so that wagersBeingVerified doesn't
+    //         slowly bloat into tons of data that no one needs
+
+    //DJSFIXME else (if myid is nowhere)
+    //DJSFIXME then emit InvalidWager(myid);
+    //DJSFIXME then do nothing?
+  }
+
+  function verifyWager(uint _wagerIndex) public {
+    Wager memory wagerToVerify = wagers[msg.sender][_wagerIndex];
+    //DJSFIXME concat the smartScaleID from the wager onto the URL
+    //DJSFIXME if statement to verify that this wager hasn't expired.
+      //DJSFIXME If the wager has expired, delete it from the wagers
+      //DJSFIXME emit WagerExpired(msg.sender, wagerToVerify.wagerAmount);
+      //DJSFIXME If the wager has not expired, do the following.
+    bytes32 myID = oraclize_query("URL", "json(https://api.coinbase.com/v2/prices/ETH-USD/spot).data.amount");
+    wagersBeingVerified[myID] = VerifyingWager(msg.sender, _wagerIndex);
+    emit WagerBeingVerified(msg.sender, _wagerIndex);
   }
 
   function getWagers() public view returns (uint[] expirations, uint[] targets, Goals[] goals, uint[] values) {
--- test/test_WeightWagers.js
+++ test/test_WeightWagers.js
@@ -12,6 +12,33 @@ function logWatchPromise(_event) {
   });
 }
 
+//helper function for waiting for the creation and activation of wagers
+//DJSFIXME Try this at some point
+function createAndActivateWager(contract, args, txDetails) {
+
+  //Wait for create
+  const createResponse = await contract.createWager(args.exp, args.target, args.goal, args.scaleID, txDetails);
+
+  //Wait for activate
+  const logScaleWatcher = logWatchPromise(contract.WagerActivated({ fromBlock: 'latest'} ));
+  const activateResponse = await logScaleWatcher;
+
+  //Great!
+  return { createResponse, activateResponse };
+}
+
+function verifyWagerAndWaitForEvent(contract, args, txDetails, eventToWaitFor) {
+  //Verify
+  const verifyResponse = await contract.verifyWager(args.wagerIndex, txDetails);
+
+  //Promisify
+  const logScaleWatcher = logWatchPromise(contract[eventToWaitFor]({ fromBlock: 'latest'} ));
+  const eventResponse = await logScaleWatcher;
+
+  //Bye!
+  return { verifyResponse, eventResponse };
+}
+
 contract('WeightWagers', accounts => {
   const owner = accounts[0];
   const alice = accounts[1];
@@ -20,28 +47,26 @@ contract('WeightWagers', accounts => {
   it('calling createWager should emit a WagerCreated event follow by a WagerActivated event', async () => {
     const weightWagers = await WeightWagers.deployed();
 
-    let wagerActivated = false;
-
-    //Alice creates a wager
+    //Alice creates a wager.
     const response = await weightWagers.createWager(30, 210, 0, 'scaleBelongingToAlice', {from: alice});
     let log = response.logs[0];
     assert.equal(log.event, 'WagerCreated', 'WagerCreated not emitted.');
 
     //Set up listener to make sure the wager gets
-    //activated once the oracle returns data
+    //activated once the oracle returns data.
     const logScaleWatcher = logWatchPromise(weightWagers.WagerActivated({ fromBlock: 'latest'} ));
     log = await logScaleWatcher;
     assert.equal(log.event, 'WagerActivated', 'WagerActivated not emitted.');
 
     //Finally, call getWagers() for Alice to ensure
-    //that she has a wager
+    //that she has a wager.
     const aliceWagers = await weightWagers.getWagers({from: alice});
     assert.equal(aliceWagers[0][0], 30, 'alice does not have the correct expiration date on her wager');
     assert.equal(aliceWagers[1][0], 210, 'alice does not have the correct target weight');
     assert.equal(aliceWagers[2][0], 0, 'alice does not have the correct goal');
     assert.equal(aliceWagers[3][0], 0, 'alice does not have the correct amount');
 
-    //Just for a realite check, make sure bob has no wagers.
+    //Just for a reality check, make sure bob has no wagers.
     const bobWagers = await weightWagers.getWagers();
     assert.deepEqual(bobWagers[0], [], "bob's expiration dates are not an empty array");
     assert.deepEqual(bobWagers[1], [], "bob's target weights are not an empty array");
@@ -49,6 +74,55 @@ contract('WeightWagers', accounts => {
     assert.deepEqual(bobWagers[3], [], "bob's wager amounts are not an empty array");
 
   });
+  
+  //DJSFIXME delete this test after it passes a few times
+  it('verify that tests do not carry side effects from other tests', async () => {
+    const weightWagers = await WeightWagers.deployed();
+
+    //Finally, call getWagers() for Alice to ensure
+    //that she has a wager.
+    const aliceWagers = await weightWagers.getWagers({from: alice});
+    assert.deepEqual(aliceWagers[0], [], "alice's expiration dates are not an empty array");
+    assert.deepEqual(aliceWagers[1], [], "alice's target weights are not an empty array");
+    assert.deepEqual(aliceWagers[2], [], "alice's goals are not an empty array");
+    assert.deepEqual(aliceWagers[3], [], "alice's wager amounts are not an empty array");
+
+    //Just for a reality check, make sure bob has no wagers.
+    const bobWagers = await weightWagers.getWagers();
+    assert.deepEqual(bobWagers[0], [], "bob's expiration dates are not an empty array");
+    assert.deepEqual(bobWagers[1], [], "bob's target weights are not an empty array");
+    assert.deepEqual(bobWagers[2], [], "bob's goals are not an empty array");
+    assert.deepEqual(bobWagers[3], [], "bob's wager amounts are not an empty array");
+  });
+
+  it('create a wager and attempt to verify it without having lost the weight', async () => {
+    const weightWagers = await WeightWagers.deployed();
+
+    //Alice creates a wager that expires very far in the future
+    const response = await weightWagers.createWager(10000, 210, 0, 'scaleBelongingToAlice', {from: alice});
+    let log = response.logs[0];
+    assert.equal(log.event, 'WagerCreated', 'WagerCreated not emitted.');
+
+    //Set up listener so we can pause execution
+    //until wager is activated
+    const logScaleWatcher = logWatchPromise(weightWagers.WagerActivated({ fromBlock: 'latest'} ));
+    log = await logScaleWatcher;
+
+    const verifyResponse = await weightWagers.verifyWager(0, {from: alice});
+  });
+
+  it('create a wager and attempt to verify it after having lost the weight', async () => {
+    const weightWagers = await WeightWagers.deployed();
+  });
+
+  it('create a wager and attempt to verify it after it has expired', async () => {
+    const weightWagers = await WeightWagers.deployed();
+  });
+
+  it('attempt to verify a wager that does not exist', async () => {
+    const weightWagers = await WeightWagers.deployed();
+  });
+
 });
 
 
